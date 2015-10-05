76ce57ee3cbc2febd4cc53857ed653fe680b46a4
Falk Hoppe <falkhoppe81@gmail.com>
2015-10-05 19:11:46 +0200

    using card helper

--- app/helpers/card_helper.rb
+++ app/helpers/card_helper.rb
@@ -2,7 +2,7 @@ require 'component_tag'
 module CardHelper
   include ComponentTag
 
-  component_tag 'card'
-  component_tag 'card_header'
+  component_tag 'card', 'article'
+  component_tag 'card_header', 'header'
   component_tag 'card_body'
 end
--- app/helpers/placement_helper.rb
+++ app/helpers/placement_helper.rb
@@ -2,7 +2,7 @@ require 'component_tag'
 module PlacementHelper
   include ComponentTag
 
-  component_tag 'placement', 'article'
+  component_tag 'placement', 'section'
   component_tag 'placement_header', 'header'
   component_tag 'placement_body'
 end
